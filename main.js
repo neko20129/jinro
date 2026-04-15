625157dc8ee39364e97deed3a0775c4a6c938a3d
neko20129 <minecraft.nekosan@outlook.jp>
2026-04-15 22:34:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+document.querySelectorAll('.title-btn').forEach(e => e.addEventListener('click', () => changeScreen('title-scr')));
+document.getElementById('create-btn').addEventListener('click', () => changeScreen('create-scr'));
+document.getElementById('join-btn').addEventListener('click', () => changeScreen('join-scr'));
+document.getElementById('roles-btn').addEventListener('click', () => changeScreen('roles-scr'));
 
+function changeScreen(scrId) {
+    const screens = document.querySelectorAll('.screen');
+    screens.forEach(screen => screen.style.display = 'none');
+    const targetScreen = document.getElementById(scrId);
+    if (targetScreen) {
+        targetScreen.style.display = 'flex';
+    }
+}
